fada6662da28d03be08c6c2d837367620c80e7c1
Owen Radcliffe <owen.radcliffe@globe.com>
2026-07-14 15:04:35 -0400

    Qa/stress testing post endpoint (#23)
    * Added peak load test
    
    * Fixing string formatting
    
    * Using symlink
    
    * Added stress test script

---
+++ tests/siege_stress.sh
@@ -0,0 +1,12 @@
+# timestamp=$(date +"%Y%m%d-%H%M%S")
+# log_file="results/siege_baseline-${timestamp}.log"
+# mkdir -p results
+
+echo "Word counts for test articles:"
+wc -w data/*
+echo ""
+
+echo "Running siege stress test for POST /audio/speech..."
+export SIEGERC="siege.conf"
+siege -C
+siege -c 25 -t 15M --internet --content-type="application/json" -f urls/stress-urls.txt
