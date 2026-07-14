6ccbd2a22e468c7d0de56162956a8566d5252157
Owen Radcliffe <owen.radcliffe@globe.com>
2026-07-14 13:53:09 -0400

    Setting params for realistic baseline testing

--- tests/siege_baseline.sh
+++ tests/siege_baseline.sh
@@ -9,4 +9,4 @@ echo ""
 echo "Running siege baseline test for POST /audio/speech..."
 export SIEGERC="siege.conf"
 siege -C
-siege -c 1 -r 1 --internet --content-type="application/json" -f urls/baseline-urls.txt
+siege -c 2 -r 5 -d 300.0 --internet --content-type="application/json" -f urls/baseline-urls.txt
