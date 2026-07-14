cfea455e23f05335e5e95e1f32d902ecdc786edb
Owen Radcliffe <owen.radcliffe@globe.com>
2026-07-14 08:50:44 -0400

    Baseline siege script implemented

--- tests/siege_baseline.sh
+++ tests/siege_baseline.sh
@@ -1,13 +1,14 @@
 timestamp=$(date +"%Y%m%d-%H%M%S")
 log_file="results/siege_baseline-${timestamp}.log"
+mkdir -p results
 
 echo "Word counts for test articles:" >> "$log_file" 2>&1
 wc -w data/* >> "$log_file" 2>&1
 echo "" >> "$log_file" 2>&1
 
-mkdir -p results
-siege -c 1 -r once -v --internet --content-type="application/json" -f urls/post-urls.txt >> "$log_file" 2>&1
+echo "Seeding data for siege baseline test..." >> "$log_file" 2>&1
+siege -c 5 -r once -v --content-type="application/json" -f urls/post-urls.txt >> "$log_file" 2>&1
 
 echo "" >> "$log_file" 2>&1
 
-siege -c 1 -r once -v --internet -f urls/get-urls.txt >> "$log_file" 2>&1
+siege -c 5 -t 10M -v -f urls/get-urls.txt >> "$log_file" 2>&1
