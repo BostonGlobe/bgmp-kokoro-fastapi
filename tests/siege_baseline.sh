06be56c26fce06c544d1e5a276a6b0b760671444
Owen Radcliffe <owen.radcliffe@globe.com>
2026-07-14 12:00:39 -0400

    Created Jenkinsfile for load testing

--- tests/siege_baseline.sh
+++ tests/siege_baseline.sh
@@ -1,10 +1,10 @@
-timestamp=$(date +"%Y%m%d-%H%M%S")
-log_file="results/siege_baseline-${timestamp}.log"
-mkdir -p results
+# timestamp=$(date +"%Y%m%d-%H%M%S")
+# log_file="results/siege_baseline-${timestamp}.log"
+# mkdir -p results
 
-echo "Word counts for test articles:" >> "$log_file" 2>&1
-wc -w data/* >> "$log_file" 2>&1
-echo "" >> "$log_file" 2>&1
+echo "Word counts for test articles:"
+wc -w data/*
+echo ""
 
-echo "Running siege baseline test for POST /audio/speech..." >> "$log_file" 2>&1
-siege -c 2 -r 5 -d 300.0 --internet --content-type="application/json" -f urls/baseline-urls.txt >> "$log_file" 2>&1
+echo "Running siege baseline test for POST /audio/speech..."
+siege -c 2 -r 1 -d 300.0 --internet --content-type="application/json" -f urls/baseline-urls.txt
