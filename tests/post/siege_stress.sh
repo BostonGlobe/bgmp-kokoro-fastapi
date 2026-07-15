aeb381a2a5199383b1116871711e2dd4ea7092f5
Owen Radcliffe <owen.radcliffe@globe.com>
2026-07-15 15:40:51 -0400

    Incrementing concurrent users in stress test

--- tests/post/siege_stress.sh
+++ tests/post/siege_stress.sh
@@ -1,4 +1,4 @@
 echo "Running siege stress test for POST /audio/speech..."
 export SIEGERC="siege.conf"
 siege -C
-siege -c 10 -r 1 --internet --content-type="application/json" -f urls/stress-urls.txt
+siege -c 15 -r 1 --internet --content-type="application/json" -f urls/stress-urls.txt
