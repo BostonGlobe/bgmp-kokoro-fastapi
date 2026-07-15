b1d222776b6849cc84bcba26bbd2a219ad7b7382
Owen Radcliffe <owen.radcliffe@globe.com>
2026-07-15 11:52:25 -0400

    Updating filepaths

--- tests/get/siege_peak.sh
+++ tests/get/siege_peak.sh
@@ -1,5 +1,5 @@
 echo "Running siege peak test for GET /download/{filename}..."
 export SIEGERC="siege.conf"
 siege -C
-siege -c 5 -r 8 --internet --content-type="application/json" -f ../urls/get-urls.txt
+siege -c 5 -r 8 --internet --content-type="application/json" -f urls/get-urls.txt
 cat siege.log
--- tests/get/siege_stress.sh
+++ tests/get/siege_stress.sh
@@ -1,5 +1,5 @@
 echo "Running siege stress test for GET /download/{filename}..."
 export SIEGERC="siege.conf"
 siege -C
-siege -c 25 -t 15M --internet --content-type="application/json" -f ../urls/get-urls.txt
+siege -c 25 -t 15M --internet --content-type="application/json" -f urls/get-urls.txt
 cat siege.log
